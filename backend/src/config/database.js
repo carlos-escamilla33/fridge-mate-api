ec080bb020d3e64dea6c9dc87b65ef87fd9748be
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2025-09-03 18:54:38 -0700

    created jest tests for database connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,13 +1,13 @@
-const {Pool} = require('pg');
-const dotenv = require('dotenv');
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-export default pool;
+module.exports = pool;
---
+++ backend/tests/models/database.test.js
@@ -0,0 +1,17 @@
+const pool = require("../../src/config/database");
+
+test("Checking connection to database", async () => {
+  const res = await pool.query("SELECT NOW()");
+  expect(res).toBeDefined();
+  expect(res.rows.length).toBe(1);
+});
+
+test("The connection to database fails", async () => {
+    await expect(pool.query("SELECT * FROM nonexistantTable"))
+        .rejects
+        .toThrow();
+});
+
+afterAll(async () => {
+    await pool.end();
+})
