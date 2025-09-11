1b9ee64ffeeaf76fd044b7979b1277bc29a261fe
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-11 12:38:08 -0700

    completed createRecipe model function

---
+++ backend/src/models/recipeModel.js
@@ -0,0 +1,23 @@
+const {pool} = require("../config/database");
+
+const createRecipe = async ({account_id, recipe_name, ingredients, instructions, based_on_items}) =>{
+    try {
+        const {rows: [recipe]} = await pool.query(
+            `
+            INSERT INTO recipe(account_id, recipe_name, ingredients, instructions, based_on_items)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *; 
+            `,
+            [account_id, recipe_name, ingredients, instructions, based_on_items]
+        );
+
+        return recipe;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    createRecipe,
+
+}
