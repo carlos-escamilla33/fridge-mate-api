f8c839bafd668e9312a60c033bcdbae246caf70b
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-11 10:17:03 -0700

    completed createProfile model function

--- backend/src/models/profileModel.js
+++ backend/src/models/profileModel.js
@@ -2,7 +2,16 @@ const pool = require("../config/database");
 
 const createProfile = async ({account_id, first_name, last_name, notifcations_enabled}) => {
     try {
-        
+        const {rows: [profile]} = await pool.query(
+            `
+            INSERT INTO profile(account_id, first_name, last_name, notifications_enabled)
+            VALUES($1, $2, $3, $4)
+            RETURNING *;
+            `,
+            [account_id, first_name, last_name, notifcations_enabled]
+        );
+
+        return profile;
     } catch (err) {
         throw err;
     }
@@ -10,5 +19,4 @@ const createProfile = async ({account_id, first_name, last_name, notifcations_en
 
 module.exports = {
     createProfile,
-
 }
