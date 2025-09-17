bb3a638c40e198a140ae884a6f829f4a50d849c3
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-17 12:32:50 -0700

    working on auth route and controllers

--- backend/src/routes/publicRoutes/auth.js
+++ backend/src/routes/publicRoutes/auth.js
@@ -2,8 +2,10 @@ const express = require("express");
 const authRouter = express.Router();
 const {} = require("../controllers/authController");
 
-authRouter.use((req, res) => {
+authRouter.use((req, res, next) => {
     console.log("A request has been made to /auth");
+
+    next();
 });
 
 authRouter.post("/register", createAccount);
