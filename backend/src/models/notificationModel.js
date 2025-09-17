104cc657c92e5933a47d1287dd8acfed455329be
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-16 22:31:15 -0700

    completed markNotificationAsRead model function

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -33,7 +33,44 @@ const findByNotificationsByProfileId = async (profile_id) => {
     }
 }
 
+const findUnreadNotificationsByProfileId = async (profile_id) => {
+    try {
+        const {rows} = await pool.query(
+            `
+            SELECT * FROM notification
+            WHERE profile_id=$1
+            AND is_read=$2
+            `,
+            [profile_id, false]
+        );
+
+        return rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const markNotificationAsRead = async (notification_id, profile_id) => {
+    try {
+        const {rows: [notification]} = await pool.query(
+            `
+            UPDATE notification
+            SET is_read=$1
+            WHERE notification_id=$2 AND profile_id=$3
+            RETURNING *;
+            `,
+            [true, notification_id, profile_id]
+        );
+
+        return notification;
+    } catch (err) {
+        throw err;
+    }
+}
+
 module.exports = {
     createNotification,
-    findByNotificationsByProfileId
+    findByNotificationsByProfileId,
+    findUnreadNotificationsByProfileId,
+    markNotificationAsRead
 }
