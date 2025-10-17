d3ca58cd92bf5bb2b188561d2a96d896e48808c1
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-10-17 16:11:40 -0700

    completed getSingleProfile profileController

--- src/controllers/profilesController.js
+++ src/controllers/profilesController.js
@@ -1,10 +1,9 @@
-const {findProfilesByAccountId} = require("../database/models/profileModel");
+const {findProfilesByAccountId, findProfileById} = require("../database/models/profileModel");
 
 
 const getAllAccountProfiles = async (req, res, next) => {
     const {id} = req.user;
     try {
-        console.log(id);
         const profiles = await findProfilesByAccountId(id);
 
         return res.send({
@@ -15,6 +14,31 @@ const getAllAccountProfiles = async (req, res, next) => {
     }
 }
 
+const getSingleProfile = async (req, res, next) => {
+    const {id} = req.params;
+    const accountId = req.user.id;
+    try {
+        const profile = await findProfileById(id);
+
+        if (!profile) {
+            return res.status(404).json({message: "Account not found"});
+        }
+
+        if (profile.account_id !== accountId) {
+            return res.status(403).json({ 
+                message: "Access denied - this profile doesn't belong to you" 
+            });
+        }
+
+        res.send({
+            profile
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
-    getAllAccountProfiles
+    getAllAccountProfiles,
+    getSingleProfile,
 }
--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const profileRouter = express.Router();
 const {authenticateToken} = require("../middleware/authToken");
-const {getAllAccountProfiles} = require("../controllers/profilesController");
+const {getAllAccountProfiles, getSingleProfile} = require("../controllers/profilesController");
 
 profileRouter.use((req, res, next) => {
     console.log("A request has been made to /profiles");
@@ -9,6 +9,7 @@ profileRouter.use((req, res, next) => {
 });
 
 profileRouter.get("/", authenticateToken, getAllAccountProfiles);
+profileRouter.get("/:id", authenticateToken, getSingleProfile);
 
 module.exports = {
     profileRouter
